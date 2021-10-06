6c32a77b6967b490356d524f2bc77a94ed36a05c
Hamid Oujand <hmdrzaa11@gmail.com>
2021-10-07 00:54:18 +0330

    add 404 route handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import ApiError from "./utils/ApiError";
 let app = express();
 
 app.get("/", (req, res) => {
   res.send({
     msg: "some thing cool",
-    nodeEnv: process.env.NODE_ENV,
   });
+});
+
+app.all("*", (req, res, next) => {
+  let err = new ApiError(`Can't find "${req.originalUrl}"`, 404);
+  next(err);
 });
 
 export default app;
