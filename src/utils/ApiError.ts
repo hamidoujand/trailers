154879913517bb1b508f0b6ff0960c40792b3561
Hamid Oujand <hmdrzaa11@gmail.com>
2021-10-07 00:50:26 +0330

    add ApiError class

---
+++ src/utils/ApiError.ts
@@ -0,0 +1,12 @@
+export default class ApiError extends Error {
+  statusCode: number;
+  status: string;
+  isOperational: boolean;
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "failed" : "error";
+    this.isOperational = true;
+    Object.setPrototypeOf(this, ApiError.prototype);
+  }
+}
