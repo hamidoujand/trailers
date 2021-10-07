9c6bdac99ed8176697269c0f9f244e428d0436d8
Hamid Oujand <hmdrzaa11@gmail.com>
2021-10-07 22:23:41 +0330

    add 'findGenres' fn to find genres

---
+++ src/utils/findGenres.ts
@@ -0,0 +1,19 @@
+import { tmdb } from "../api/tmdb";
+import { Genres } from "../types";
+
+export let findGenres = async (genresIds: number[]): Promise<string[]> => {
+  try {
+    let { data } = await tmdb.get<Genres>("/genre/movie/list");
+    let genres: string[] = [];
+    genresIds.forEach((id) => {
+      data.genres.forEach((genre) => {
+        if (genre.id === id) {
+          genres.push(genre.name);
+        }
+      });
+    });
+    return genres;
+  } catch (error) {
+    throw error;
+  }
+};
