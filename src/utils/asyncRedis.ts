cc9a76085b811d5cdc8f62d96c52383b061b2646
Hamid Oujand <hmdrzaa11@gmail.com>
2021-10-09 16:35:32 +0330

    add 'redisGet' function

--- src/utils/asyncRedis.ts
+++ src/utils/asyncRedis.ts
@@ -1,4 +1,6 @@
 import { redisClient } from "../redis";
+import { CustomMovie } from "../types";
+import ApiError from "./ApiError";
 
 export let redisSet = function (key: string, value: string): Promise<string> {
   return new Promise((resolve, reject) => {
@@ -7,6 +9,23 @@ export let redisSet = function (key: string, value: string): Promise<string> {
         return reject(err);
       }
       resolve(ok);
+    });
+  });
+};
+
+export let redisGet = function (key: string): Promise<CustomMovie> {
+  return new Promise((resolve, reject) => {
+    redisClient.get(key, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      if (data) {
+        let movie: CustomMovie = JSON.parse(data);
+        resolve(movie);
+      } else {
+        let err = new ApiError("trailer not found", 404);
+        reject(err);
+      }
     });
   });
 };
