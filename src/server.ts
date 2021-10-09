29161ca9e468b2c2987e592cbae29819fdbe8756
Hamid Oujand <hmdrzaa11@gmail.com>
2021-10-09 20:50:06 +0330

    add graceful shutdown

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,14 @@
 import app from "./app";
 import config from "./config/index";
+import { redisClient } from "./redis";
 
-app.listen(config.port, () => console.log(`listening on ${config.port}`));
+let server = app.listen(config.port, () =>
+  console.log(`listening on ${config.port}`)
+);
+
+//shutdown gracefully
+process.on("SIGTERM", () => {
+  server.close(() => {
+    redisClient.flushall();
+  });
+});
