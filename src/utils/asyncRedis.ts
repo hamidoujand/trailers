bda946d07f4c5840d870fc4d1ce1697ccdd9a9b5
Hamid Oujand <hmdrzaa11@gmail.com>
2021-10-12 19:50:23 +0330

    fix a bug in redisGet fn

--- src/utils/asyncRedis.ts
+++ src/utils/asyncRedis.ts
@@ -1,6 +1,5 @@
 import { redisClient } from "../redis";
 import { CustomMovie } from "../types";
-import ApiError from "./ApiError";
 
 export let redisSet = function (key: string, value: string): Promise<string> {
   return new Promise((resolve, reject) => {
@@ -13,18 +12,14 @@ export let redisSet = function (key: string, value: string): Promise<string> {
   });
 };
 
-export let redisGet = function (key: string): Promise<CustomMovie> {
+export let redisGet = function (key: string): Promise<CustomMovie | null> {
   return new Promise((resolve, reject) => {
     redisClient.get(key, (err, data) => {
-      if (err) {
-        return reject(err);
-      }
       if (data) {
         let movie: CustomMovie = JSON.parse(data);
         resolve(movie);
       } else {
-        let err = new ApiError("trailer not found", 404);
-        reject(err);
+        resolve(null);
       }
     });
   });
