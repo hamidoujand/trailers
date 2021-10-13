5fd14fbd4f5ee0f40e5f08afdb704a55276ca920
Hamid Oujand <hmdrzaa11@gmail.com>
2021-10-13 16:16:21 +0330

    add full react client

---
+++ client/src/components/Loader/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+export default function index() {
+  return (
+    <div>
+      <div className="lds-ellipsis">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
